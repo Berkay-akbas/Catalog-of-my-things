383a0495958537ec201120da632c233dcea41969
adebowale <ademibowaleadegboye@gmail.com>
2022-10-11 10:21:36 +0100

    fix linters errror for better clean code

--- genre.rb
+++ genre.rb
@@ -1,22 +1,22 @@
 require 'securerandom'
 
 class Genre
-          attr_accessor :id, :name, :items
+  attr_accessor :id, :name, :items
 
-def initialize(name)
-  @id = Securerandom.hex(5)
-  @name = name 
-  @items = []
-end           
+  def initialize(name)
+    @id = Securerandom.hex(5)
+    @name = name
+    @items = []
+  end
 
-def to_json(*_args)
-  "{
+  def to_json(*_args)
+    "{
     \"id\": \"#{id}\",
-    \"name\":\"#{name}\" 
-     }"        
-end
+    \"name\":\"#{name}\"
+     }"
+  end
 
-def add_items(item)
-  @items << item
+  def add_items(item)
+    @items << item
+  end
 end
-end        
