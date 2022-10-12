2a144e14cfc2f81ed7534f1bb76b78b0d14307cb
adebowale <ademibowaleadegboye@gmail.com>
2022-10-12 12:13:59 +0100

    checkong for the testing

--- spec/genre.spec.rb
+++ spec/genre.spec.rb
@@ -4,3 +4,8 @@ describe Genre do
   before :each do
     @genre = Genre.new('Fiction Series')
   end
+
+  context 'When testing the Genre class' do
+          it 'Should create an instance of the Genre class' do
+            expect(@genre).to be_an_instance_of Genre
+          end
