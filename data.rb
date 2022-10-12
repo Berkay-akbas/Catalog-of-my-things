87d7721680d1acb392af9561570f5afe8322a427
adebowale <ademibowaleadegboye@gmail.com>
2022-10-12 18:00:08 +0100

    add test

--- data.rb
+++ data.rb
@@ -8,6 +8,10 @@ def save_album(album)
   JSON.dump(genre, File.open('./storage/genre.json', 'w+'))
  end
 
+ def save_labels(label)
+  JSON.dump(label, File.open('./storage/label.json', 'w+'))
+end
+
  def save_source(source)
   JSON.dump(source, File.open('./storage/source.json', 'w+'))
  end
@@ -16,4 +20,5 @@ def save_album(album)
   save_album(item.music_albums)
   save_genre(item.genres)
   save_source(item.sources)
+  save_labels(item.labels)
  end
