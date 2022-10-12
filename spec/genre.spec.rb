78856ef9b9d34d8f4f0f546aee8a118c559d45a0
adebowale <ademibowaleadegboye@gmail.com>
2022-10-12 12:22:33 +0100

    fix linters

--- spec/genre.spec.rb
+++ spec/genre.spec.rb
@@ -6,15 +6,13 @@ describe Genre do
   end
 
   context 'When testing the Genre class' do
-          it 'Should create an instance of the Genre class' do
-            expect(@genre).to be_an_instance_of Genre
-          end
-
-          
-    it 'Should add a genre to the items' do
-          @genre.add_items('Fiction Series')
-          expect(@genre.items).not_to be_empty
-        end
-      end
+    it 'Should create an instance of the Genre class' do
+      expect(@genre).to be_an_instance_of Genre
     end
-    
+
+    it 'Should add a genre to the items' do
+      @genre.add_items('Fiction Series')
+      expect(@genre.items).not_to be_empty
+    end
+  end
+end
