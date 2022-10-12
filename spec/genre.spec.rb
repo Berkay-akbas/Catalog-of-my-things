f5974fcb58864aff1701750d3a2ff2352055e211
adebowale <ademibowaleadegboye@gmail.com>
2022-10-12 12:15:09 +0100

    testing add item

--- spec/genre.spec.rb
+++ spec/genre.spec.rb
@@ -9,3 +9,12 @@ describe Genre do
           it 'Should create an instance of the Genre class' do
             expect(@genre).to be_an_instance_of Genre
           end
+
+          
+    it 'Should add a genre to the items' do
+          @genre.add_items('Fiction Series')
+          expect(@genre.items).not_to be_empty
+        end
+      end
+    end
+    
