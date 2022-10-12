7ed16ecac8d350dad5465b17f5c4ced8778deb71
adebowale <ademibowaleadegboye@gmail.com>
2022-10-12 10:45:36 +0100

    fix linters

--- preserve_data.rb
+++ preserve_data.rb
@@ -3,17 +3,17 @@ require 'json'
 def retrieve_albums(music_albums)
   data = JSON.parse(File.read('./storage/album'))
   data.each do |album|
-          music_albums << MusicAlbum.new(album['on_spotify'],
-          album['publish_date'])
-          end
-    end
+    music_albums << MusicAlbum.new(album['on_spotify'],
+                                   album['publish_date'])
+  end
+end
 
 def retrieve_genres(genres)
   data = JSON.parse(file.read('./storage/genre.json'))
   data.each do |genre|
     genres << Genre.new(genre['name'])
   end
-end              
+end
 
 retrieve_albums(item.music_albums)
 retrieve_genres(item.genres)
