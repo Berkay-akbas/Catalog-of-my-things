cc854552c76e123209297f3a0151e9325185903a
adebowale <ademibowaleadegboye@gmail.com>
2022-10-13 10:50:54 +0100

    fix linters

--- music.rb
+++ music.rb
@@ -3,15 +3,15 @@ require './item'
 class MusicAlbum < Item
   attr_accessor :on_spotify, :publish_date
 
-  def initialize(on_spotify, _publish_date)
-    super(public_date)
+  def initialize(on_spotify, publish_date)
+    super(publish_date)
     @on_spotify = on_spotify
   end
 
   def to_json(*_args)
     "{
-     \"on_spotify\": \"#{on_spotify}\",
-     \"publish_date\":\"#{public_date}\"
+      \"on_spotify\": \"#{on_spotify}\",
+      \"publish_date\": \"#{publish_date}\"
     }"
   end
 
